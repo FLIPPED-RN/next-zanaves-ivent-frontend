4e90bbc8b869b814f76b4159c32b7d3f3083a7ee
FLIPPED <rnazarov142@gmail.com>
2025-04-29 17:43:08 +0200

    v2.0.2

--- app/api/send-to-telegram/route.ts
+++ app/api/send-to-telegram/route.ts
@@ -32,7 +32,6 @@ export async function POST(request: Request) {
 
     return NextResponse.json({ success: true })
   } catch (error) {
-    console.error('Error:', error)
     return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: 500 }
