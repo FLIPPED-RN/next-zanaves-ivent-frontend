50590b432c28a4c8f61b1b67194b6e9c1f374d0b
FLIPPED <rnazarov142@gmail.com>
2025-05-22 22:32:18 +0200

    add new link for my channel

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <div className="border-t-1 border-amber-400 py-10">
-            <p className="text-center">Powered by <a href="https://t.me/fast_loverr" className="text-[#C68C2A]">Ruslan Nazarov</a></p>
+            <p className="text-center">Powered by <a href="https://t.me/flipped_rn" className="text-[#C68C2A]">FLIPPED-RN</a></p>
         </div>
     )
 }
